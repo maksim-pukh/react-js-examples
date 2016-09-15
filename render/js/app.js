6ec244e74adef759c3abe50411ca960ebb5e8260
maksim-pukh <!ignore67%>
2016-09-15 14:01:56 +0700

    Добавил пример на добавление элемента React'а в DOM

---
+++ render/js/app.js
@@ -0,0 +1,19 @@
+var Greetings = React.createClass({
+    render: function() {
+        // какие-нибудь действия
+
+        return (
+            // все возвращаемые элементы должны быть обернуты div'ом
+            // свойство class html элементов тут задается как className, дабы избежать конфликтов с class из js
+            <div className='root-class'>
+                <div>Hello, world!</div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    // обязателен закрывающий тег
+    <Greetings/>,
+    document.getElementsByClassName('js-react')[0]
+);
